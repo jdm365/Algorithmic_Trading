8217b94ccdc8a4be9948bcdb8ef7301d460a3621
jdm365 <jdm365@georgetown.edu>
2022-01-10 20:03:24 -0500

    Worked on STJGCN

--- RL STJGCN/Model_Main.py
+++ RL STJGCN/Model_Main.py
@@ -7,7 +7,7 @@ from torch.nn import opt
 
 
 class GraphConstructor(nn.Module):
-    def __init__(self, n_nodes, n_features, time_features=15*5*4*12, delta_min=0.05):
+    def __init__(self, n_nodes, n_features, lookback_window, time_features=15*5*4*12, delta_min=0.05):
         super(GraphConstructor, self).__init__()
         ### 
         # n_nodes: int - number of nodes/assets
@@ -16,7 +16,8 @@ class GraphConstructor(nn.Module):
         # delta_min: float - minimum weight to consider in adjacency matrices
 
         self.n_nodes = n_nodes
-        self.layer_initial = nn.init.xavier_normal((n_nodes, 1))
+        self.layer_initial = nn.init.xavier_normal((n_nodes, 1, lookback_window))
+        self.lookback_window = lookback_window
         self.n_features = n_features
         self.time_features = time_features
         self.delta_min = delta_min
@@ -42,17 +43,39 @@ class GraphConstructor(nn.Module):
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def create_node_embedding(self, space, time):
+    def one_hot_encode_temporal(self, observation):
         ###
-        # space: Tensor (n_nodes, 1) - Spatial node embedding
-        # time: Tensor (n_nodes * time_features, 1) - Temporal node embedding
-        if space is None:
-            space = self.layer_initial
-        return self.spatial(space) + self.temporal(time)
+        # observation: Tensor - (n_nodes, input_features + 1, lookback_window)
+        # timestamps: Tensor - (n_nodes, lookback_window)
 
-    def create_adjacency_matrix(self, space, time1, time2):
-        U1 = self.create_node_embedding(space, time1)
-        U2 = self.create_node_embedding(space, time2)
+        # output: Tensor (n_nodes, time_features, lookback_window)
+        timestamps = T.squeeze(observation[:, -1, :])
+        encoded_times = F.one_hot(timestamps) ## write after getting data
+        return encoded_times
+
+    def create_node_embedding(self, observation):
+        ###
+        # observation: Tensor - (n_nodes, input_features + 1, lookback_window)
+        # space: Tensor (n_nodes, 1, lookback_window) - Spatial node embedding
+        # time: Tensor (n_nodes * time_features, lookback_window) - Temporal node embedding
+
+        # output: Tensor (n_nodes, n_features, lookback_window)
+        space = self.layer_initial
+        time = self.one_hot_encode_temporal(observation)
+        return self.spatial(self.layer_initial) + self.temporal(time)
+
+    def create_adjacency_matrix(self, observation, idx, time_diff):
+        ###
+        # observation: Tensor (n_nodes, input_features + 1, lookback_window)
+        # idx: int - current time step
+        # U: Tensor (n_nodes, n_features, lookback_window) - spatio-temporal node embeddings
+        # time_diff: int - time difference between spatial embedding 1 and 2
+
+        # output: Tensor (n_nodes, n_nodes)
+        U = self.create_node_embedding(observation)
+
+        U1 = T.squeeze(U[:, :, idx - time_diff])
+        U2 = T.squeeze(U[:, :, idx])
         x = T.mm(T.mm(U1, self.B), T.transpose(U2, 0, 1))
         x = T.tensor([i if i >= self.delta_min else 0 \
             for i in T.flatten(x)]).reshape(x.shape)
@@ -61,8 +84,25 @@ class GraphConstructor(nn.Module):
 
 class DilatedGraphConvolutionCell(nn.Module):
     def __init__(self, kernel_size, n_data_features, n_output_features, 
-    dilation_list, fc1_dims, fc2_dims, n_features, n_nodes, window_size):
+    dilation_list, fc1_dims, fc2_dims, n_features, n_nodes, lookback_window):
         super(DilatedGraphConvolutionCell, self).__init__()
+        ### 
+        # kernel_size: int - size of kernel for convolution
+        # n_data_features: int - number of node features in observed data
+        # n_output_features: int - features after convolution
+        # dilation_list: list - list of dilation factors for each layer
+        # fc1_dims: int
+        # fc2_dims: int
+        # n_features: int - number of nodes features after FC layers
+        # n_nodes: int
+        # lookback_window: int
+        
+        # self.W_forward: Tensor (kernel_size, n_features, n_output_features) - parameter used in 
+        #                                                                       convolutional layers
+        # self.W_backward: Tensor (kernel_size, n_features, n_output_features) - parameter used in 
+        #                                                                       convolutional layers
+        # self.b: Tensor (n_output_features) - parameter used in convolutional layers
+
         self.kernel_size = kernel_size
         self.n_data_features = n_data_features
         self.n_output_features = n_output_features
@@ -71,19 +111,19 @@ class DilatedGraphConvolutionCell(nn.Module):
         self.fc2_dims = fc2_dims
         self.n_features = n_features
         self.n_nodes = n_nodes
-        self.window_size = window_size
+        self.lookback_window = lookback_window
 
         self.FC = nn.Sequential(
             nn.Linear(n_nodes * n_data_features, fc1_dims),
             F.relu(),
             nn.Linear(fc1_dims, fc2_dims),
             F.relu(),
-            nn.Linear(fc2_dims, n_features),
+            nn.Linear(fc2_dims, n_nodes * n_features),
             F.relu()
         )
 
-        self.W_forward = nn.Parameter(T.ones((n_features, n_output_features)))
-        self.W_backward = nn.Parameter(T.ones((n_features, n_output_features)))
+        self.W_forward = nn.Parameter(T.ones((self.kernel_size, n_features, n_output_features)))
+        self.W_backward = nn.Parameter(T.ones((self.kernel_size, n_features, n_output_features)))
         self.b = nn.Parameter(T.ones((n_output_features)))
 
         self.optimizer = T.optim.Adam(self.parameters(), lr=1e-4)
@@ -91,36 +131,130 @@ class DilatedGraphConvolutionCell(nn.Module):
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def normalize_degree_matrix(self, space, time1, time2):
+    def normalize_adjacency_matrix(self, observation, idx, time_diff):
+        ###
+        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
+        # idx: int - current time step
+        # time_diff: int - difference between timesteps for ST graph construction
+
+        # output: Tensor (n_nodes, n_nodes) - normalized adjacency matrix
         graph = GraphConstructor(
             self.n_nodes, 
-            self.n_features
+            self.n_features, 
+            self.lookback_window
         )
-        adjacency_matrix = graph.create_adjacency_matrix(space, time1, time2)
+        adjacency_matrix = graph.create_adjacency_matrix(observation, idx, time_diff)
         degree_matrix = adjacency_matrix.sum(-1)
         return T.mm(T.mm(T.pow(degree_matrix, -1/2), adjacency_matrix), T.pow(degree_matrix, 1/2))
 
-    def conv(self, space, time):
+    def conv(self, observation, idx):
+        ###
+        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
+        # idx: int - current time step
+        # X: Tensor (n_nodes, n_features, lookback_window)
+
+        # output: Tensor (n_nodes, n_output_features, 1) - output of convolution operation
         Z = T.zeros((self.n_nodes, self.n_output_features))
         for k in range(self.kernel_size):
-            L1 = self.normalize_degree_matrix(space, time - k, time)
-            L2 = self.normalize_degree_matrix(space, time - k, time)
-            X = self.FC(space)
-            x = T.mm(T.mm(L1, X), self.W_forward) + T.mm(T.mm(L2, X), self.W_backward) \
+            L1 = self.normalize_adjacency_matrix(observation, idx, k)
+            L2 = self.normalize_adjacency_matrix(observation, idx, -k)
+            X = self.FC(observation)[:, :, idx - k]
+            x = T.mm(T.mm(L1, X), T.squeeze(self.W_forward[k, :, :])) \
+                + T.mm(T.mm(L2, X), T.squeeze(self.W_backward[k, :, :])) \
                 + self.b
             Z += F.relu(x)
+        return Z.reshape(*Z.shape, 1)
+
+    def conv_layer(self, input, dilation_factor):
+        ###
+        # input: Tensor (n_nodes, n_features, lookback_window)
+        # dilation_factor: int - dilation for conv layer
+
+        # output: Tensor (n_nodes, n_output_features, lookback_window) - output of convolution operation
+        for t in range(self.lookback_window):
+            if t % dilation_factor == 0:
+                try:
+                    Z = T.cat(Z, self.conv(input, t), dim=-1)
+                except:
+                    Z = self.conv(input, t)
+            else:
+                try:
+                    Z = T.cat(Z, T.zeros((self.n_nodes, self.n_output_features, 1)), dim=-1)
+                except:
+                    Z = T.zeros((self.n_nodes, self.n_output_features, 1))
         return Z
 
-    def conv_layer(self, space, time, dilation_factor):
-        for t in range(time - self.window_size, time):
-            Z = self.conv(space, t)
+    def STJGN_module(self, observation):
+        ###
+        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
 
-    def STJGN_layer(self, space, time):
-        layer = nn.Sequential(
-            self.conv_layer(space, time, self.dilation_list[0]),
-            self.conv_layer(space, time, self.dilation_list[1]),
-            self.conv_layer(space, time, self.dilation_list[2]),
-            self.conv_layer(space, time, self.dilation_list[3])
+        # output: list - hidden states of each STJGCN layer
+        Z0 = self.conv_layer(observation, self.dilation_list[0])
+        Z1 = self.conv_layer(Z0, self.dilation_list[1])
+        Z2 = self.conv_layer(Z1, self.dilation_list[2])
+        Z3 = self.conv_layer(Z2, self.dilation_list[3])
+
+        output = [Z0[:, :, -1], Z1[:, :, -1], Z2[:, :, -1], Z3[:, :, -1]]
+        return output
+
+
+class AttentionOutputModule(nn.Module):
+    def __init__(self, kernel_size, n_data_features, n_output_features, 
+    dilation_list, fc1_dims, fc2_dims, n_features, n_nodes, lookback_window):
+        super(AttentionOutputModule, self).__init__()
+        ### 
+        # kernel_size: int - size of kernel for convolution
+        # n_data_features: int - number of node features in observed data
+        # n_output_features: int - features after convolution
+        # dilation_list: list - list of dilation factors for each layer
+        # fc1_dims: int
+        # fc2_dims: int
+        # n_features: int - number of nodes features after FC layers
+        # n_nodes: int
+        # lookback_window: int
+
+        # self.W: Tensor (n_features, n_features) - parameter used in 
+        #                                           calculating attention weights
+        # self.v: Tensor (n_features) - parameter used in calculating attention weights
+        # self.b: Tensor (n_features) - parameter used in calculating attention weights
+
+        self.kernel_size = kernel_size
+        self.n_data_features = n_data_features
+        self.n_output_features = n_output_features
+        self.dilation_list = dilation_list
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_features = n_features
+        self.n_nodes = n_nodes
+        self.lookback_window = lookback_window
+
+        self.STJGCN = DilatedGraphConvolutionCell(
+            kernel_size=kernel_size,
+            n_data_features=n_data_features,
+            n_output_features=n_output_features,
+            dilation_list=dilation_list,
+            fc1_dims=fc1_dims,
+            fc2_dims=fc2_dims,
+            n_features=n_features,
+            n_nodes=n_nodes,
+            lookback_window=lookback_window
         )
 
+        self.W = nn.Parameter(T.zeros(n_features, n_features))
+        self.b = nn.Parameter(T.zeros(n_features))
+        self.v = nn.Parameter(T.zeros(n_features))
+        self.optimizer = T.optim.Adam(self.parameters(), lr=1e-4)
 
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def compute_att_weighted_conv_output(self, observation):
+        hidden_states = self.STJGCN.STJGN_module(observation)
+        Z = T.sum(T.exp(hidden_states))
+        for state in hidden_states:
+            s = self.v.transpose * T.tanh(T.mm(self.W, state) + self.b)
+            alpha = T.exp(s) / Z
+        return T.dot(alpha, hidden_states)
+
+    def forward(self, observation):
+        
