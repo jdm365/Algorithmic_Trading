b42a65d4c3d53dd1743cc4f59f4f4ff15d1e0f62
jdm365 <jdm365@georgetown.edu>
2022-01-12 17:05:40 -0500

    Finished first pass with data; haven't tested yet

--- RL STJGCN/Model_Main.py
+++ RL STJGCN/Model_Main.py
@@ -6,9 +6,11 @@ import numpy as np
 from torch.nn import opt
 import pandas as pd
 import tqdm
+import os
+import datetime
 
 class GraphConstructor(nn.Module):
-    def __init__(self, n_nodes, n_features, lookback_window, time_features=15*5*4*12, delta_min=0.05):
+    def __init__(self, n_nodes, n_features, lookback_window, time_features=15+5+4+12, delta_min=0.05):
         super(GraphConstructor, self).__init__()
         ### 
         # n_nodes: int - number of nodes/assets
@@ -44,28 +46,15 @@ class GraphConstructor(nn.Module):
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def one_hot_encode_temporal(self, observation):
+    def create_node_embedding(self, time_features):
         ###
         # observation: Tensor - (n_nodes, input_features + 1, lookback_window)
-        # timestamps: Tensor - (n_nodes, lookback_window)
-
-        # output: Tensor (n_nodes, time_features, lookback_window)
-        timestamps = T.squeeze(observation[:, -1, :])
-        encoded_times = F.one_hot(timestamps) ## write after getting data
-        return encoded_times
-
-    def create_node_embedding(self, observation):
-        ###
-        # observation: Tensor - (n_nodes, input_features + 1, lookback_window)
-        # space: Tensor (n_nodes, 1, lookback_window) - Spatial node embedding
-        # time: Tensor (n_nodes * time_features, lookback_window) - Temporal node embedding
+        # time_features: Tensor (n_nodes * time_features, lookback_window) - Temporal node embedding
 
         # output: Tensor (n_nodes, n_features, lookback_window)
-        space = self.layer_initial
-        time = self.one_hot_encode_temporal(observation)
-        return self.spatial(self.layer_initial) + self.temporal(time)
+        return self.spatial(self.layer_initial) + self.temporal(time_features)
 
-    def create_adjacency_matrix(self, observation, idx, time_diff):
+    def create_adjacency_matrix(self, time_features, idx, time_diff):
         ###
         # observation: Tensor (n_nodes, input_features + 1, lookback_window)
         # idx: int - current time step
@@ -73,7 +62,7 @@ class GraphConstructor(nn.Module):
         # time_diff: int - time difference between spatial embedding 1 and 2
 
         # output: Tensor (n_nodes, n_nodes)
-        U = self.create_node_embedding(observation)
+        U = self.create_node_embedding(time_features)
 
         U1 = T.squeeze(U[:, :, idx - time_diff])
         U2 = T.squeeze(U[:, :, idx])
@@ -132,9 +121,9 @@ class DilatedGraphConvolutionCell(nn.Module):
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def normalize_adjacency_matrix(self, observation, idx, time_diff):
+    def normalize_adjacency_matrix(self, time_features, idx, time_diff):
         ###
-        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
+        # time_features: Tensor (n_nodes, 15+5+4+12, lookback_window)
         # idx: int - current time step
         # time_diff: int - difference between timesteps for ST graph construction
 
@@ -144,21 +133,22 @@ class DilatedGraphConvolutionCell(nn.Module):
             self.n_features, 
             self.lookback_window
         )
-        adjacency_matrix = graph.create_adjacency_matrix(observation, idx, time_diff)
+        adjacency_matrix = graph.create_adjacency_matrix(time_features, idx, time_diff)
         degree_matrix = adjacency_matrix.sum(-1)
         return T.mm(T.mm(T.pow(degree_matrix, -1/2), adjacency_matrix), T.pow(degree_matrix, 1/2))
 
-    def conv(self, observation, idx):
+    def conv(self, observation, time_features, idx):
         ###
-        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
+        # observation: Tensor (n_nodes, n_data_features, lookback_window)
+        # time_features: Tensor (n_nodes, 15+5+4+12, lookback_window)
         # idx: int - current time step
         # X: Tensor (n_nodes, n_features, lookback_window)
 
         # output: Tensor (n_nodes, n_output_features, 1) - output of convolution operation
         Z = T.zeros((self.n_nodes, self.n_output_features))
         for k in range(self.kernel_size):
-            L1 = self.normalize_adjacency_matrix(observation, idx, k)
-            L2 = self.normalize_adjacency_matrix(observation, idx, -k)
+            L1 = self.normalize_adjacency_matrix(time_features, idx, k)
+            L2 = self.normalize_adjacency_matrix(time_features, idx, -k)
             X = self.FC(observation)[:, :, idx - k]
             x = T.mm(T.mm(L1, X), T.squeeze(self.W_forward[k, :, :])) \
                 + T.mm(T.mm(L2, X), T.squeeze(self.W_backward[k, :, :])) \
@@ -166,18 +156,19 @@ class DilatedGraphConvolutionCell(nn.Module):
             Z += F.relu(x)
         return Z.reshape(*Z.shape, 1)
 
-    def conv_layer(self, input, dilation_factor):
+    def conv_layer(self, input, time_features, dilation_factor):
         ###
-        # input: Tensor (n_nodes, n_features, lookback_window)
+        # input: Tensor (n_nodes, n_data_features, lookback_window)
+        # time_features: Tensor (n_nodes, 15+5+4+12, lookback_window)
         # dilation_factor: int - dilation for conv layer
 
         # output: Tensor (n_nodes, n_output_features, lookback_window) - output of convolution operation
         for t in range(self.lookback_window):
             if t % dilation_factor == 0:
                 try:
-                    Z = T.cat(Z, self.conv(input, t), dim=-1)
+                    Z = T.cat(Z, self.conv(input, time_features, t), dim=-1)
                 except:
-                    Z = self.conv(input, t)
+                    Z = self.conv(input, time_features, t)
             else:
                 try:
                     Z = T.cat(Z, T.zeros((self.n_nodes, self.n_output_features, 1)), dim=-1)
@@ -185,12 +176,13 @@ class DilatedGraphConvolutionCell(nn.Module):
                     Z = T.zeros((self.n_nodes, self.n_output_features, 1))
         return Z
 
-    def STJGN_module(self, observation):
+    def STJGN_module(self, observation, time_features):
         ###
-        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
+        # observation: Tensor (n_nodes, n_data_features, lookback_window)
+        # time_features: Tensor (n_nodes, 15+5+4+12, lookback_window)
 
         # output: list - hidden states of each STJGCN layer
-        Z0 = self.conv_layer(observation, self.dilation_list[0])
+        Z0 = self.conv_layer(observation, time_features, self.dilation_list[0])
         Z1 = self.conv_layer(Z0, self.dilation_list[1])
         Z2 = self.conv_layer(Z1, self.dilation_list[2])
         Z3 = self.conv_layer(Z2, self.dilation_list[3])
@@ -258,37 +250,37 @@ class AttentionOutputModule(nn.Module):
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def compute_att_weighted_conv_output(self, observation):
+    def compute_att_weighted_conv_output(self, observation, time_features):
         ###
-        # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
+        # observation: Tensor (n_nodes, n_data_features, lookback_window)
+        # time_features: Tensor (n_nodes, 15+5+4+12, lookback_window)
         
         # output: Tensor (n_nodes, n_features)
-        hidden_states = self.STJGCN.STJGN_module(observation)
+        hidden_states = self.STJGCN.STJGN_module(observation, time_features)
         Z = T.sum(T.exp(hidden_states))
         for state in hidden_states:
             s = self.v.transpose * T.tanh(T.mm(self.W, state) + self.b)
             alpha = T.exp(s) / Z
         return T.dot(alpha, hidden_states)
 
-    def forward(self, observation, last_action):
+    def forward(self, observation, time_features, last_action):
         ###
         # observation: Tensor (n_nodes, n_data_features + 1, lookback_window)
         # last_action: Tensor (n_nodes) - last action (previous portfolio weights)
         
         # output: Tensor (n_nodes) - action (new portfloio weights)
-        Y = self.compute_att_weighted_conv_output(observation)
+        Y = self.compute_att_weighted_conv_output(observation, time_features)
         out = T.cat(self.state_layer(Y), self.last_action_layer(last_action))
         return self.FC(out)
 
 
 class Agent(nn.Module):
-    def __init__(self, filename):
+    def __init__(self):
         super(Agent, self).__init__()
         ###
         # minibatch_size: int
         # filename: location of market data
         self.minibatch_size = 128
-        self.filename = filename
         self.network = AttentionOutputModule(
             kernel_size=2, 
             n_data_features=4, 
@@ -306,37 +298,76 @@ class Agent(nn.Module):
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def create_global(self):
-        ###
-        # time_initial: int - time step index of current state
-
-        # output: Tensor (n_nodes, n_data_features + 1, lookback_window) - current state
-
-        DF = pd.read_csv(self.filename)
-        ## Create global price tensor
-        ## Will create when I get data
-
-    def step(self, observation, last_action):
-        action = self.network.forward(observation, last_action)
-        price_change_vector = observation[:, 0, -1]
+    def step(self, observation, time_features, last_action):
+        action = self.network.forward(observation, time_features, last_action)
+        price_change_vector = observation[:, 2, -1]
         reward = T.dot(last_action, price_change_vector) / self.minibatch_size
         return action, reward
 
-        
+
+class GetData():
+    def __init__(self, filepath='/Users/jakemehlman/Desktop/Minute_Data_v1/'):
+        self.filepath = filepath
+
+    def make_DF(self):
+        DFNEW = pd.DataFrame()
+        for filename in os.listdir(self.filepath):
+            DF = pd.read_csv(self.filepath + filename)
+            DF = DF[['Local time', 'High', 'Low', 'Close', 'Volume']]
+            try:
+                DFNEW = DFNEW.merge(DF, left_on='Local time', right_on='Local time')
+            except:
+                DFNEW = DF
+        DFNEW = DFNEW.replace(0, 1)
+        return DFNEW
+
+    def make_global_tensor_no_time(self):
+        df = self.make_DF()
+        arr = np.array(df)[:, 1:] # (time, features)
+        Arr = np.array(arr[1:, :] / arr[:-1, :]).astype(float)
+        X = T.ones((Arr.shape[-1] // 4, 4, Arr.shape[0]))
+        for i in range(arr.shape[-1]):
+            j = i // 4
+            k = i % 4
+            X[j, k, :] = T.tensor(Arr[:, i])
+        return X
+
+    def make_global_temporal_tensor(self):
+        df = self.make_DF()
+        arr = np.array(df)[:, 0]
+        M = T.zeros((arr.shape[0], 36))
+        for i in range(1, arr.shape[0]):
+            base = arr[i]
+
+            half_hour = T.tensor(2 * (int(base[11:13]) - 9) + (int(base[14:16]) >= 30))
+            day = T.tensor(datetime.strptime(base[:10].replace('.', ' '), '%d %m %Y').isoweekday())
+            week = T.tensor(abs(int(base[:2]) - 4) // 7)
+            month = T.tensor(int(base[3:5]))
+
+            half_hour = F.one_hot(half_hour-1, 15)
+            day = F.one_hot(day-1, 5)
+            week = F.one_hot(week, 4)
+            month = F.one_hot(month-1, 12)
+            M[i, :] = T.cat((half_hour, day, week, month))
+        return M[1:, :]
+
+
 if __name__ == '__main__':
     n_epochs = 1000
-    agent = Agent(filename='FileLocation')
-    X = Agent.create_global()
+    agent = Agent()
+    X = GetData().make_global_tensor_no_time()
+    M = GetData().make_global_temporal_tensor()
     for epoch in tqdm(range(n_epochs)):
         done = False
         time_initial = np.random.randint(agent.network.lookback_window, \
-            len(Agent.create_global().shape(-1)) - agent.minibatch_size)
+            X.shape(-1) - agent.minibatch_size)
         Reward = 0
         cntr = 0
         capital = 10000
         while done is False:
             observation = X[:, :, time_initial + cntr - agent.network.lookback_window:cntr + time_initial]
-            last_action, reward = agent.step(observation, last_action)
+            time_feature = M[time_initial + cntr - agent.network.lookback_window:cntr + time_initial, :]
+            last_action, reward = agent.step(observation, time_feature, last_action)
             Reward += reward
             capital *= T.exp(reward * agent.minibatch_size)
             cntr += 1
