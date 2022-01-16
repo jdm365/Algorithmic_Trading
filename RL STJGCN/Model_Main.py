12ab5ab87d0b899c8e58759079d5abf440ec7b39
jdm365 <jdm365@georgetown.edu>
2022-01-16 16:15:30 -0500

    Added mixed precision functionality

--- RL STJGCN/Model_Main.py
+++ RL STJGCN/Model_Main.py
@@ -414,6 +414,7 @@ if __name__ == '__main__':
         lookback_window=64,
         minibatch_size=16
     )
+    scaler = T.cuda.amp.GradScaler()
     for epoch in tqdm(range(n_epochs)):
         done = False
         time_initial = np.random.randint(agent.network.lookback_window, \
@@ -423,20 +424,24 @@ if __name__ == '__main__':
         capital = 10000
         last_action = nn.Softmax()(T.rand(X.shape[0])).to('cuda:0' if T.cuda.is_available() else 'cpu')
         while done is False:
-            observation = X[:, :, time_initial + cntr - agent.network.lookback_window:cntr + time_initial]
-            time_features = M[time_initial + cntr - agent.network.lookback_window:cntr + time_initial, :]
-            last_action, reward = agent.step(observation, time_features, last_action)
-            Reward += reward
-            capital *= T.exp(reward * agent.minibatch_size)
-            cntr += 1
-            if cntr % agent.minibatch_size == 0:
-                done = True
+            with T.cuda.amp.autocast():
+                observation = X[:, :, time_initial + cntr - agent.network.lookback_window:cntr + time_initial]
+                time_features = M[time_initial + cntr - agent.network.lookback_window:cntr + time_initial, :]
+                last_action, reward = agent.step(observation, time_features, last_action)
+                Reward += reward
+                capital *= T.exp(reward * agent.minibatch_size)
+                cntr += 1
+                if cntr % agent.minibatch_size == 0:
+                    done = True
         Loss = -Reward
-        Loss.backward()
-        agent.optimizer.step()
-        agent.network.optimizer.step()
-        agent.network.STJGCN.optimizer.step()
-        agent.network.STJGCN.graph.optimizer.step()
+        scaler.scale(Loss).backward()
+        
+        scaler.step(agent.optimizer)
+        scaler.step(agent.network.optimizer)
+        scaler.step(agent.network.STJGCN.optimizer)
+        scaler.step(agent.network.STJGCN.graph.optimizer)
+        
+        scaler.update()
         agent.optimizer.zero_grad()
         agent.network.optimizer.zero_grad()
         agent.network.STJGCN.optimizer.zero_grad()
