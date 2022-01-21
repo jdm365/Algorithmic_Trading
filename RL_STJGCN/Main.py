bcdc694c9d40f4b0185f98961b7a6a4c1319ed89
jdm365 <jdm365@georgetown.edu>
2022-01-20 22:03:16 -0500

    Added buy and hold to utils and main function. Also added Trained Models directory

--- RL_STJGCN/Main.py
+++ RL_STJGCN/Main.py
@@ -5,7 +5,7 @@ from torch._C import device
 from tqdm import tqdm
 from RL_STJGCN.Long_Only import GetData, Agent
 from RL_STJGCN.Long_Short import Agent as ShortAgent
-from utils import plot_learning
+from utils import plot_learning, BuyAndHold
 
 class Trainer():
     def __init__(self, trade_frequency, minibatch_size=30, long_only=True, cuda=False):
@@ -51,7 +51,9 @@ class Trainer():
                 margin=self.margin,
                 n_time_features=self.n_time_features
             )
+        bnh_agent = Agent(X)
         Profit_History = []
+        Relative_Profit_History = []
         for epoch in tqdm(range(n_epochs)):
             done = False
             time_initial = np.random.randint(agent.network.lookback_window, \
@@ -59,6 +61,7 @@ class Trainer():
             Reward = 0
             cntr = 0
             capital = 10000
+            bnh_capital = 10000
             last_action = (self.margin * ((T.rand(X.shape[0])).softmax(dim=0))).to(self.device)
             if self.long_only:
                 last_action = (T.rand(X.shape[0])).softmax(dim=0).to(self.device)
@@ -68,6 +71,7 @@ class Trainer():
                 last_action, reward = agent.step(observation, time_features, last_action)
                 Reward += reward
                 capital *= T.exp(reward * agent.minibatch_size)
+                bnh_capital *= bnh_agent.step(observation)
                 cntr += 1
                 if cntr % agent.minibatch_size == 0:
                     done = True
@@ -85,19 +89,90 @@ class Trainer():
             agent.network.STJGCN.graph.optimizer.zero_grad()
 
             Profits = capital - 10000
+            BnH_Profits = bnh_capital - 10000
+            Relative_Profits = Profits - BnH_Profits
             Profit_History.append(Profits.detach().numpy())
+            Relative_Profit_History.append(Relative_Profits.detach().numpy())
             History = np.mean(Profit_History[-100:])
+            Relative_History = np.mean(Profit_History[-100:])
 
-            if epoch % 109 == 0:
+            if epoch % 100 == 0:
                 print(f'Episode profits: {History}')
+                print(f'Episode relative profits: {Relative_History}')
+
         plot_learning(Profit_History, 'Profit_History.png')
+        plot_learning(Relative_Profit_History, 'Relative_Profit_History.png')
+
         trained_model_directory = '/Trained_Models/'
         T.save(agent.state_dict(), trained_model_directory + 'Agent.pt')
         T.save(agent.network.state_dict(), trained_model_directory + 'Network.pt')
         T.save(agent.network.STJGCN.state_dict(), trained_model_directory + 'STJGCN.pt')
         T.save(agent.network.STJGCN.graph.state_dict(), trained_model_directory + 'Graph.pt')
+    
+    def test(self, run_length):
+        trained_model_directory = '/Trained_Models/'
+        shutup.please()
+        X = GetData(self.trade_frequency).make_global_tensor_no_time().to(self.device)
+        M = GetData(self.trade_frequency).make_global_temporal_tensor().to(self.device)
+        if self.long_only:
+            agent = Agent(
+                kernel_size=2, 
+                n_data_features=4, 
+                dilation_list=[2, 2, 4, 2, 2], 
+                fc1_dims=256, 
+                fc2_dims=512, 
+                n_features=64, 
+                n_nodes=X.shape[0], 
+                lookback_window=64,
+                minibatch_size=self.minibatch_size,
+                n_time_features=self.n_time_features
+            )
+        else:
+            agent = ShortAgent(
+                kernel_size=2, 
+                n_data_features=4, 
+                dilation_list=[2, 2, 4, 2, 2], 
+                fc1_dims=256, 
+                fc2_dims=512, 
+                n_features=64, 
+                n_nodes=X.shape[0], 
+                lookback_window=64,
+                minibatch_size=self.minibatch_size,
+                margin=self.margin,
+                n_time_features=self.n_time_features
+            )
+        bnh_agent = BuyAndHold(X)
+
+        T.load(agent.state_dict(), trained_model_directory + 'Agent.pt')
+        T.load(agent.network.state_dict(), trained_model_directory + 'Network.pt')
+        T.load(agent.network.STJGCN.state_dict(), trained_model_directory + 'STJGCN.pt')
+        T.load(agent.network.STJGCN.graph.state_dict(), trained_model_directory + 'Graph.pt')
+
+        done = False
+        time_initial = np.random.randint(agent.network.lookback_window, \
+            X.shape[-1] - run_length)
+        cntr = 0
+        bnh_capital = 10000
+        capital = 10000
+        last_action = (self.margin * ((T.rand(X.shape[0])).softmax(dim=0))).to(self.device)
+        if self.long_only:
+            last_action = (T.rand(X.shape[0])).softmax(dim=0).to(self.device)
+        while done is False:
+            observation = X[:, :, time_initial + cntr - agent.network.lookback_window:cntr + time_initial]
+            time_features = M[time_initial + cntr - agent.network.lookback_window:cntr + time_initial, :]
+            last_action, reward = agent.step(observation, time_features, last_action)
+            bnh_capital *= bnh_agent.step(observation)
+            capital *= T.exp(reward * agent.minibatch_size)
+            cntr += 1
+            if cntr == run_length:
+                done = True
+        Profits = 10000 - capital
+        BnH_Profits = 10000 - bnh_capital
+        print(f'Final agent profits: ${Profits}')
+        print(f'Final agent buy and hold profits: ${BnH_Profits}')
 
 if __name__ == '__main__':
     DataFrequency = ['Minute', 'Hourly']
     Train = Trainer(DataFrequency[1], cuda=True)
     Train.train(n_epochs=7500)
+    #Train.test(run_length=1500)
--- utils.py
+++ utils.py
@@ -14,3 +14,17 @@ def plot_learning(scores, filename=None, x=None, window=5):
     plt.plot(x, running_avg)
     if filename:
       plt.savefig(filename)
+
+class BuyAndHold():
+    def __init__(self, data):
+        self.inital_action = T.ones(data.shape[0], device=self.device) / data.shape[0]
+
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def step(self, observation):
+        price_change_vector = observation[:, 2, -1]
+        profit = T.dot(self.inital_action, price_change_vector)
+        return self.inital_action, profit
+
+
