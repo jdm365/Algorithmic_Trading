8dea22b7ec868e9728ef6af337e2fc9d483eec20
jdm365 <jdm365@georgetown.edu>
2022-02-02 09:30:13 -0500

    Added discrete stock trader

---
+++ GARCH_PPO/Convolutional_Reccurrent_Preprocessing/Main.py
@@ -0,0 +1,182 @@
+from GARCH_PPO.Get_Data import GetData
+from Discrete_RNN import Agent
+from Discrete_RNN import Preproccess
+import numpy as np
+from numpy import NaN, random
+from utils import plot_learning
+from tqdm import tqdm
+import torch as T
+
+def train(n_episodes=500, commission_rate=.0025, reward_type='standard', ticker='.INX'):
+    data = GetData(convolutional=True, ticker=ticker)
+    agent = Agent()
+    gamma_comm = 1#\ - commission_rate
+
+    figure_file = 'Profit_History.png'
+    profit_history = []
+    BnH_profit_history = []
+    learn_iters = 0
+    steps = 0
+
+    for i in tqdm(range(n_episodes)):
+        time_initial = random.randint(50, data.X_m.shape[0]-3072)
+        minutely_data, daily_data, weekly_data = data.create_observation(time_initial)
+        done = False
+        cash = 5000
+        equity = 5000
+        capital = cash + equity
+        cntr = 0
+        closes = []
+        hx_M = T.zeros(2, minutely_data.shape[0], 64).to(agent.preprocess.device)
+        hx_D = T.zeros(2, daily_data.shape[0], 64).to(agent.preprocess.device)
+        hx_W = T.zeros(2, weekly_data.shape[0], 64).to(agent.preprocess.device)
+        while not done:
+            steps += 1
+            initial_cash = cash
+            initial_equity = equity
+            initial_capital = cash + equity
+
+            last_close = data.X_m[time_initial + cntr - 1, -2]
+            action, prob, val, observation, hx_M, hx_D, hx_W = \
+                agent.choose_action(minutely_data, daily_data, weekly_data, hx_M, hx_D, hx_W)
+            cntr += 1
+            minutely_data, daily_data, weekly_data = data.create_observation(time_initial + cntr)
+            close = data.X_m[time_initial + cntr - 1, -2]
+
+            delta_c = ((close - last_close) / last_close) * initial_equity
+            closes.append(close)
+            running_mean_long = np.mean(closes[-21:])
+            running_mean_medium = np.mean(closes[-13:])
+            running_mean_short = np.mean(closes[-8:])
+            if action < 0:
+                cash = (initial_equity + delta_c) * -action * gamma_comm + initial_cash
+                equity = (initial_equity + delta_c) * (1 + action)
+            else:
+                cash = initial_cash * (1 - action)
+                equity = (initial_equity + delta_c) + initial_cash * action * gamma_comm
+            capital = cash + equity
+            delta_capital = (capital - initial_capital) / initial_capital
+            
+            if reward_type == 'standard':
+                reward = ((action * (close - last_close)) / last_close)
+            elif reward_type == 'momentum':
+                reward = (action * ((running_mean_medium - running_mean_long) / (running_mean_long))) + \
+                    2*(action * ((running_mean_short - running_mean_medium) / (running_mean_medium))) + \
+                    3*(action * ((last_close - running_mean_short) / (running_mean_short))) + \
+                    4*(action * ((close - last_close) / (last_close)))
+            elif reward_type == 'mean_reverting':
+                reward = (action * ((running_mean_long - last_close) / last_close)) + \
+                    ((action * (close - last_close)) / last_close)
+            elif reward_type == 'traditional':
+                reward = (capital - initial_capital)
+
+            agent.remember(observation, action, prob, val, reward, done)
+            
+            if steps % agent.N == 0:
+                agent.learn()
+                learn_iters += 1
+                done = True
+            
+        if learn_iters % 25 == 0:
+            agent.save_models(reward_type)
+        BnH_profits = ((closes[-1] / closes[2]) * 10000) - 10000
+
+        BnH_profit_history.append(BnH_profits)
+        profit_history.append(capital - 10000)
+        print('Strategy:', reward_type, 'Episode Profits: $', profit_history[-1][0],\
+            'Episode Relative Profits: $', (profit_history[-1][0] - BnH_profits).round(decimals=2),\
+            'Relative Profit History Average: $', np.round(np.mean(profit_history[-100:])\
+            - np.mean(BnH_profit_history[-100:]), decimals=2), 'n_steps:',\
+            steps, 'Learning Steps: ', learn_iters)
+
+    plot_learning(profit_history, filename=figure_file)
+    agent.save_models(reward_type)
+
+def test(steps=20000, commission_rate=0.0025, ticker='.INX', strategies=['traditional', 'mean_reverting']):
+    data = GetData(convolutional=True, ticker=ticker)
+    agent_1 = Agent()
+    agent_2 = Agent()
+    agent_1.load_models(strategies[0])
+    agent_2.load_models(strategies[1])
+    gamma_comm = 1# - commission_rate
+
+    time_initial = random.randint(32, data.X_m.shape[0]-(steps+250))
+    minutely_data, daily_data, weekly_data = data.create_observation(time_initial)
+    done = False
+
+    cash_1 = 8000
+    equity_1 = 2000
+    capital_1 = cash_1 + equity_1
+
+    cash_2 = 8000
+    equity_2 = 2000
+    capital_2 = cash_2 + equity_2
+
+    max_drawdown_1 = 0
+    capital_history_1 = []
+
+    max_drawdown_2 = 0
+    capital_history_2 = []
+
+    closes = []
+    cntr = 0
+    while not done:
+        initial_cash_1 = cash_1
+        initial_equity_1 = equity_1
+        initial_cash_2 = cash_2
+        initial_equity_2 = equity_2
+
+        last_close = data.X_m[time_initial + cntr - 1, -2]
+        action_1 = agent_1.choose_action(minutely_data, daily_data, weekly_data)[0]
+        action_2 = agent_2.choose_action(minutely_data, daily_data, weekly_data)[0]
+        cntr += 1
+        minutely_data, daily_data, weekly_data = data.create_observation(time_initial + cntr)
+        close = data.X_m[time_initial + cntr - 1, -2]
+        closes.append(close)
+
+        delta_c_1 = ((close - last_close) / last_close) * initial_equity_1
+        
+        if action_1 < 0:
+            cash_1 = (initial_equity_1 + delta_c_1) * -action_1 * gamma_comm + initial_cash_1
+            equity_1= (initial_equity_1 + delta_c_1) * (1 + action_1)
+        else:
+            cash_1 = initial_cash_1 * (1 - action_1)
+            equity_1 = (initial_equity_1 + delta_c_1) + initial_cash_1 * action_1 * gamma_comm
+        capital_1 = cash_1 + equity_1
+
+        capital_history_1.append(capital_1)
+        if capital_1 == min(capital_history_1):
+            max_drawdown_1 = capital_1[0] - 10000
+
+        delta_c_2 = ((close - last_close) / last_close) * initial_equity_2
+
+        if action_2 < 0:
+            cash_2 = (initial_equity_2 + delta_c_2) * -action_2 * gamma_comm + initial_cash_2
+            equity_2 = (initial_equity_2 + delta_c_2) * (1 + action_2)
+        else:
+            cash_2 = initial_cash_2 * (1 - action_2)
+            equity_2 = (initial_equity_2 + delta_c_2) + initial_cash_2 * action_2 * gamma_comm
+        capital_2 = cash_2 + equity_2
+
+        capital_history_2.append(capital_2)
+        if capital_2 == min(capital_history_2):
+            max_drawdown_2 = capital_2[0] - 10000
+
+        if cntr >= steps:
+            done = True
+    print(f'Total {strategy[0]} Profits: $', np.round((capital_1-10000)[0], decimals=2), \
+        'Max Drawdown $', np.round(max_drawdown_1, decimals=2))
+    print(f'Total {strategy[1]} Profits: $', np.round((capital_2-10000)[0], decimals=2), \
+        'Max Drawdown $', np.round(max_drawdown_2, decimals=2))
+    print('Total Buy and Hold Profits: $', np.round(10000 * (closes[-1] / closes[0]) \
+        - 10000, decimals=2))
+
+
+if __name__ == '__main__':
+    strategies = ['standard', 'momentum', 'mean_reverting']
+    for strategy in strategies:
+        train(n_episodes=500, reward_type=strategy, ticker='.INX2')
+    
+    n_backtests = 5
+    for _ in range(n_backtests):
+        test(ticker='.INX2', strategies=strategies[1:3])
---
+++ GARCH_PPO/Discrete_RNN/PPO_RAgent.py
@@ -0,0 +1,245 @@
+from torch.distributions import Categorical
+import torch as T
+from torch.cuda import is_available
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torch.optim as optim
+from torch.distributions import Normal
+from pathlib import Path
+
+class PPOMemory:
+    def __init__(self, batch_size):
+        self.states = []
+        self.probs = []
+        self.vals = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+
+        self.batch_size = batch_size
+
+    def generate_batches(self):
+        n_states = len(self.states)
+        batch_start = np.arange(0, n_states, self.batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
+        batches = [indices[i: i + self.batch_size] for i in batch_start]
+        return np.array(self.states),\
+                np.array(self.actions),\
+                np.array(self.probs),\
+                np.array(self.vals),\
+                np.array(self.rewards),\
+                np.array(self.dones),\
+                batches
+
+    def store_memory(self, state, action, probs, vals, reward, done):
+        self.states.append(state.cpu().detach().numpy())
+        self.actions.append(action)
+        self.probs.append(probs)
+        self.vals.append(vals)
+        self.rewards.append(reward)
+        self.dones.append(done)
+
+    def clear_memory(self):
+        self.states = []
+        self.probs = []
+        self.vals = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+
+
+class Preproccess(nn.Module):
+    def __init__(self, input_dims_minutely, input_dims_daily, input_dims_weekly, lr=3e-4):
+        super(Preproccess, self).__init__()
+        self.filepath = str(Path(__file__).parent)
+        self.checkpoint_dir =  self.filepath + '/Trained_Models'
+        self.filename = 'preproccess.pt'
+
+        self.MGRU = nn.GRU(input_size=input_dims_minutely[0], hidden_size=64, num_layers=2, batch_first=True)
+        self.DGRU = nn.GRU(input_size=input_dims_daily[0], hidden_size=64, num_layers=2, batch_first=True)
+        self.WGRU = nn.GRU(input_size=input_dims_weekly[0], hidden_size=64, num_layers=2, batch_first=True)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, minutely_data, daily_data, weekly_data, hx_M, hx_D, hx_W):
+        min = minutely_data.flatten(start_dim=-2).permute(0, 2, 1).contiguous()
+        day = daily_data.flatten(start_dim=-2).permute(0, 2, 1).contiguous()
+        week = weekly_data.flatten(start_dim=-2).permute(0, 2, 1).contiguous()
+
+        M, hx_M = self.MGRU(min, hx_M)
+        D, hx_D = self.DGRU(day, hx_D)
+        W, hx_W = self.WGRU(week, hx_W)
+        return T.cat((M[:,-1], D[:,-1], W[:,-1]), dim=-1), hx_M, hx_D, hx_W
+    
+    def save_checkpoint(self, reward_type):
+        T.save(self.state_dict(), self.checkpoint_dir + '/' + reward_type + '_' + self.filename)
+
+    def load_checkpoint(self, reward_type):
+        self.load_state_dict(T.load(self.checkpoint_dir + '/' + reward_type + '_' + self.filename))
+
+class ActorNetwork(nn.Module):
+    def __init__(self, input_dims, actor_lr, fc1_dims=256, fc2_dims=256, n_actions=3):
+        super(ActorNetwork, self).__init__()
+        self.filepath = str(Path(__file__).parent)
+        self.checkpoint_dir =  self.filepath + '/Trained_Models'
+        self.filename = 'actor_model.pt'
+        
+        self.actor_network = nn.Sequential(
+            nn.Linear(input_dims, fc1_dims),
+            nn.LayerNorm(fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.LayerNorm(fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, n_actions),
+            nn.Softmax(dim=-1)
+        )
+        self.optimizer = optim.Adam(self.parameters(), lr=actor_lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        dist = self.actor_network(state)
+        dist = Categorical(dist)
+        return dist
+
+    def save_checkpoint(self, reward_type):
+        T.save(self.state_dict(), self.checkpoint_dir + '/' + reward_type + '_' + self.filename)
+
+    def load_checkpoint(self, reward_type):
+        self.load_state_dict(T.load(self.checkpoint_dir + '/' + reward_type + '_' + self.filename))
+
+class CriticNetwork(nn.Module):
+    def __init__(self, input_dims, critic_lr, fc1_dims=256, fc2_dims=256):
+        super(CriticNetwork, self).__init__()
+        self.filepath = str(Path(__file__).parent)
+        self.checkpoint_dir =  self.filepath + '/Trained_Models'
+        self.filename = 'critic_model.pt'
+
+        self.critic_network = nn.Sequential(
+            nn.Linear(input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, 1)
+        )
+        self.optimizer = T.optim.Adam(self.parameters(), lr=critic_lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        return self.critic_network(state)
+
+    def save_checkpoint(self, reward_type):
+        T.save(self.state_dict(), self.checkpoint_dir + '/' + reward_type + '_' + self.filename)
+
+    def load_checkpoint(self, reward_type):
+        self.load_state_dict(T.load(self.checkpoint_dir + '/' + reward_type + '_' + self.filename))
+
+class Agent:
+    def __init__(self, input_dims_actorcritic=192, input_dims_minutely=(4,48), 
+        input_dims_daily=(5,30), input_dims_weekly=(4,30), discount=0.99, 
+        actor_lr=3e-4, critic_lr=3e-4, gae_lambda=0.95, policy_clip=0.1, 
+        batch_size=512, N=1024, n_epochs=8):
+        self.discount = discount
+        self.policy_clip = policy_clip
+        self.n_epochs = n_epochs
+        self.gae_lambda = gae_lambda
+        self.N = N
+
+        self.preprocess = Preproccess(input_dims_minutely, input_dims_daily, input_dims_weekly)
+        self.actor = ActorNetwork(input_dims_actorcritic, actor_lr)
+        self.critic = CriticNetwork(input_dims_actorcritic, critic_lr)
+        self.memory = PPOMemory(batch_size)
+
+    def remember(self, state, action, probs, vals, reward, done):
+        self.memory.store_memory(state, action, probs, vals, reward, done)
+
+    def choose_action(self, minutely_data, daily_data, weekly_data, hx_M, hx_D, hx_W):
+        self.preprocess.eval()
+        self.actor.eval()
+        self.critic.eval()
+        
+        observation, hx_M, hx_D, hx_W = self.preprocess.forward(minutely_data, daily_data, weekly_data, hx_M, hx_D, hx_W)
+        state = observation.to(self.actor.device)
+
+        dist = self.actor(state)
+        value = self.critic(state)
+        action = dist.sample()
+
+        probs = T.squeeze(dist.log_prob(action)).item()
+        action = T.squeeze(action).item() - 1
+        value = T.squeeze(value).item()
+
+        self.preprocess.train()
+        self.actor.train()
+        self.critic.train()
+        return action, probs, value, state, hx_M, hx_D, hx_W
+
+    def learn(self):
+        for _ in range(self.n_epochs):
+            states_arr, actions_arr, old_probs_arr, vals_arr, rewards_arr,\
+                dones_arr, batches = self.memory.generate_batches()
+            
+            advantage = np.zeros(len(rewards_arr), dtype=np.float32)
+
+            for t in range(len(rewards_arr) - 1):
+                discount = 1
+                a_t = 0
+                for k in range(t, len(rewards_arr) - 1):
+                    a_t += discount * (rewards_arr[k] + self.discount * vals_arr[k+1] * \
+                        (1 - int(dones_arr[k])) - vals_arr[k])
+                    discount *= self.discount * self.gae_lambda
+                advantage[t] = a_t
+            advantage = T.tensor(advantage).to(self.actor.device)
+
+            values = T.tensor(vals_arr).to(self.actor.device)
+            for batch in batches:
+                states = T.nan_to_num(T.tensor(states_arr[batch], dtype=T.float), nan=2e-1).to(self.actor.device)
+                old_probs = T.tensor(old_probs_arr[batch]).to(self.actor.device)
+                actions = T.tensor(actions_arr[batch]).to(self.actor.device)
+
+                dist = self.actor(states)
+                critic_value = self.critic(states)
+
+                critic_value = T.squeeze(self.critic(states))
+                new_probs = dist.log_probs(actions)
+                prob_ratios = T.exp(new_probs - old_probs)
+
+                weighted_probs = advantage[batch] * prob_ratios
+                weighted_clipped_probs =T.clamp(prob_ratios, 1 - self.policy_clip, 
+                    1 + self.policy_clip) * advantage[batch]
+                actor_loss = -T.min(weighted_probs, weighted_clipped_probs).mean()
+
+                returns = advantage[batch] + values[batch]
+                critic_loss = (returns - critic_value) ** 2
+                critic_loss = critic_loss.mean()
+
+                total_loss = actor_loss + 0.5 * critic_loss
+
+                self.preprocess.optimizer.zero_grad()
+                self.actor.optimizer.zero_grad()
+                self.critic.optimizer.zero_grad()
+
+                total_loss.backward()
+
+                self.preprocess.optimizer.step()
+                self.actor.optimizer.step()
+                self.critic.optimizer.step()
+
+        self.memory.clear_memory()
+
+    def save_models(self, reward_type):
+        print('...saving models...')
+        self.preprocess.save_checkpoint(reward_type)
+        self.actor.save_checkpoint(reward_type)
+        self.critic.save_checkpoint(reward_type)
+
+    def load_models(self, reward_type):
+        print('...loading models...')
+        self.preprocess.load_checkpoint(reward_type)
+        self.actor.load_checkpoint(reward_type)
+        self.critic.load_checkpoint(reward_type)
